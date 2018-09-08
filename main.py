bbbbc46dd295f014d8264e0853e1db8531a0b99e
Alper Ozaydin <alper.ozaydin@uni-konstanz.de>
2018-09-08 23:36:27 +0300

    Added on sleep time feature

--- main.py
+++ main.py
@@ -28,8 +28,7 @@ for line in output:
 dates.sort(reverse=False)
 
 wake_and_sleep = []
-for index, line in enumerate(dates):
-    print line
+for line in dates:
     date = None
     matches = datefinder.find_dates(line)
     date_in_seconds = None
@@ -48,19 +47,15 @@ for index, line in enumerate(dates):
     if "Sleep" in line:
         wake_and_sleep.append(("Sleep", date_in_seconds))
 
-print "========"
-
 total_awake_time = 0.0
 total_sleep_time = 0.0
-total_time = 0.0
 
 for i in range(0, len(wake_and_sleep)):
     try:
         if wake_and_sleep[i][0] == "Wake" and wake_and_sleep[i + 1][0] == "Sleep":
-            if wake_and_sleep[i + 1][1] > wake_and_sleep[i][1]:
-                total_awake_time = total_awake_time + (wake_and_sleep[i + 1][1] - wake_and_sleep[i][1])
-            else:
-                total_awake_time = total_awake_time + (86400.0 - wake_and_sleep[i][1])
+            total_awake_time = total_awake_time + (wake_and_sleep[i + 1][1] - wake_and_sleep[i][1])
+        else:
+            total_sleep_time = total_sleep_time + (wake_and_sleep[i + 1][1] - wake_and_sleep[i][1])
     except:
         pass
 
@@ -68,21 +63,6 @@ if len(wake_and_sleep) > 1 and wake_and_sleep[-1][0] == "Wake":
     now = time.strptime(str(datetime.datetime.now()).split(".")[0], '%Y-%m-%d %H:%M:%S')
     date_in_seconds_now = datetime.timedelta(days=now.tm_yday, hours=now.tm_hour, minutes=now.tm_min, seconds=int(now.tm_sec)).total_seconds()
     total_awake_time = total_awake_time + date_in_seconds_now - wake_and_sleep[-1][1]
-    print "{0:.2f}".format(total_awake_time/3600.0), "hours"
+    print "On use:", str(datetime.timedelta(seconds=total_awake_time)), "| On sleep:", str(datetime.timedelta(seconds=total_sleep_time))
 else:
     print "No data!"
-
-
-# date1 = "29/11/2015 21:38:02"
-# date2 = "30/11/2015 00:45:14"
-# newdate1 = time.strptime(date1, "%d/%m/%Y %H:%M:%S")
-# newdate2 = time.strptime(date2, "%d/%m/%Y %H:%M:%S")
-#
-# print newdate1
-# print newdate2
-#
-# date_in_seconds_now = datetime.timedelta(days=newdate1.tm_yday, hours=newdate1.tm_hour, minutes=newdate1.tm_min, seconds=int(newdate1.tm_sec)).total_seconds()
-# date_in_seconds_now2 = datetime.timedelta(days=newdate2.tm_yday, hours=newdate2.tm_hour, minutes=newdate2.tm_min, seconds=int(newdate2.tm_sec)).total_seconds()
-#
-# print date_in_seconds_now
-# print date_in_seconds_now2 - date_in_seconds_now

e07f1790a884ecf60c649a059c6dcd144262cc30
Alper Ozaydin <alper.ozaydin@uni-konstanz.de>
2018-09-08 23:21:47 +0300

    Added 365 days + current date

--- main.py
+++ main.py
@@ -41,8 +41,7 @@ for index, line in enumerate(dates):
         date_day = str(match).split(" ")[0]
         date_day_and_time = date_day + " " + date_time
         x = time.strptime(date_day_and_time, "%Y-%m-%d %H:%M:%S")
-        date_in_seconds = datetime.timedelta(hours=x.tm_hour, minutes=x.tm_min, seconds=x.tm_sec).total_seconds()
-        print date_in_seconds
+        date_in_seconds = datetime.timedelta(days=x.tm_yday, hours=x.tm_hour, minutes=x.tm_min, seconds=x.tm_sec).total_seconds()
         break
     if "Wake" in line:
         wake_and_sleep.append(("Wake", date_in_seconds))
@@ -54,6 +53,7 @@ print "========"
 total_awake_time = 0.0
 total_sleep_time = 0.0
 total_time = 0.0
+
 for i in range(0, len(wake_and_sleep)):
     try:
         if wake_and_sleep[i][0] == "Wake" and wake_and_sleep[i + 1][0] == "Sleep":
@@ -64,9 +64,25 @@ for i in range(0, len(wake_and_sleep)):
     except:
         pass
 
-now = time.strptime(str(datetime.datetime.now()).split(".")[0], '%Y-%m-%d %H:%M:%S')
-date_in_seconds_now = datetime.timedelta(hours=x.tm_hour, minutes=x.tm_min, seconds=int(x.tm_sec)).total_seconds()
-total_awake_time = total_awake_time + date_in_seconds_now
+if len(wake_and_sleep) > 1 and wake_and_sleep[-1][0] == "Wake":
+    now = time.strptime(str(datetime.datetime.now()).split(".")[0], '%Y-%m-%d %H:%M:%S')
+    date_in_seconds_now = datetime.timedelta(days=now.tm_yday, hours=now.tm_hour, minutes=now.tm_min, seconds=int(now.tm_sec)).total_seconds()
+    total_awake_time = total_awake_time + date_in_seconds_now - wake_and_sleep[-1][1]
+    print "{0:.2f}".format(total_awake_time/3600.0), "hours"
+else:
+    print "No data!"
 
-print "{0:.2f}".format(total_awake_time/3600.0), "hours"
 
+# date1 = "29/11/2015 21:38:02"
+# date2 = "30/11/2015 00:45:14"
+# newdate1 = time.strptime(date1, "%d/%m/%Y %H:%M:%S")
+# newdate2 = time.strptime(date2, "%d/%m/%Y %H:%M:%S")
+#
+# print newdate1
+# print newdate2
+#
+# date_in_seconds_now = datetime.timedelta(days=newdate1.tm_yday, hours=newdate1.tm_hour, minutes=newdate1.tm_min, seconds=int(newdate1.tm_sec)).total_seconds()
+# date_in_seconds_now2 = datetime.timedelta(days=newdate2.tm_yday, hours=newdate2.tm_hour, minutes=newdate2.tm_min, seconds=int(newdate2.tm_sec)).total_seconds()
+#
+# print date_in_seconds_now
+# print date_in_seconds_now2 - date_in_seconds_now
